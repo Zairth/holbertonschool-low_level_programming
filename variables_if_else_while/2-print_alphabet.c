d07a49d5c6d1572faa65aae92f2bae794dad9718
Zairth <mano.gimenez@hotmail.com>
2024-10-21 14:39:05 +0000

    g

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n;
 
-	char alphabet[26] = ["a","b","c","d","e","f","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+	char alphabet[26] = {"a","b","c","d","e","f","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 	int a_length = alphabet.length;
 	for (n = 0; n < a_length; n++)
 		putchar(alphabet[n]);
