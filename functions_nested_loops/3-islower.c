a9f4ff22c75336efe7e2edf24f44dd8c3c4c840e
Zairth <mano.gimenez@hotmail.com>
2024-10-23 07:46:28 +0000

    g

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _islower - checks for lowercase character
+  *
+  * Return: void
+  */
+int _islower(int c)
+{
+	if (_islower(c))
+		return (0);
+	else
+		return (1);
+}
---
+++ functions_nested_loops/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c = 'R';
+	_islower(c);
+	return (0);
+}
