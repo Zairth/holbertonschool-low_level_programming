60b65c393665966c5e39a4544a3252b584ed344c
Zairth <mano.gimenez@hotmail.com>
2024-10-24 19:08:08 +0000

    g

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -18,36 +18,37 @@ void print_times_table(int n)
 				result = i * j;
 				if (result >= 100)
 				{
+					if (j != n + 1)
+						_putchar(' ');
 					_putchar((result / 100) + '0');
 					_putchar(((result / 10) % 10) + '0');
 					_putchar((result % 10) + '0');
 					if (j != n)
-					{
 						_putchar(',');
-						_putchar(' ');
-					}
 				}
 				else if (result >= 10)
 				{
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-					if (j != n)
+					if (j != n + 1)
 					{
-						_putchar(',');
 						_putchar(' ');
 						_putchar(' ');
 					}
+					_putchar((result / 10) + '0');
+                                        _putchar((result % 10) + '0');
+					if (j != n)
+						_putchar(',');
 				}
 				else
 				{
-					_putchar(result + '0');
-					if (j != n)
+					if (j != n + 1)
 					{
-						_putchar(',');
 						_putchar(' ');
 						_putchar(' ');
 						_putchar(' ');
 					}
+					_putchar(result + '0');
+					if (j != n)
+						_putchar(',');
 				}
 			}
 			_putchar('\n');
