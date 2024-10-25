974dba9a735b54ccca93d54d83ccfcb44e2c1c9b
Zairth <mano.gimenez@hotmail.com>
2024-10-25 08:36:46 +0000

    g

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
-*multiple_of_3_5 - print the sum of all the multiple of 3 and 5 below 1024
-*@void: no variable
-*Return: no return
-*/
+ * multiple_of_3_5 - print the sum of all the multiple of 3 and 5 below 1024
+ *@void: no variable
+ *Return: no return
+**/
 void multiple_of_3_5(void)
 {
 	int i, sum;
@@ -14,7 +14,16 @@ void multiple_of_3_5(void)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
-
 	}
 	printf("%d\n", sum);
+}
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	multiple_of_3_5();
+	return (0);
 }
