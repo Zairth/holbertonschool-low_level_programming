c3d62669befd0f407df40aa378d41034638de707
Zairth <mano.gimenez@hotmail.com>
2024-10-25 07:59:46 +0000

    g

---
+++ functions_nested_loops/101-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	multiple_of_3_5();
+    	return (0);
+}
---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_abs - puts the absolute value of a number
+*@n: variable that contain the number to convert
+*Return: always return 0
+*/
+void multiple_of_3_5(void)
+{
+	
+}
