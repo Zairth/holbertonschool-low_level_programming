e4f0b80aad188ddecc96406630639fe5e030ee04
Zairth <mano.gimenez@hotmail.com>
2024-10-27 12:08:51 +0000

    g

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 	unsigned long int n0 = 0, n1 = 1;
 	unsigned long int result = 0, sum = 0;
 
-	while (result <= 4000000)
+	while (result < 4000000)
 	{
 		result = n0 + n1;
 		n0 = n1;
@@ -17,7 +17,7 @@ int main(void)
 		if (result % 2 == 0)
 			sum += result;
 	}
-	printf("%lu", result);
+	printf("%lu", sum);
 	printf("\n");
 	return (0);
 }
