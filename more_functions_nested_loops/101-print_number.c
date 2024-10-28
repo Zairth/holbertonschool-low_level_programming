413d95124e940ece975c16ff879cc54f16dde36b
Zairth <mano.gimenez@hotmail.com>
2024-10-28 15:50:09 +0000

    g

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
 *print_number - prints an integer.
 *@n: the integer to print
@@ -5,5 +7,28 @@
 */
 void print_number(int n)
 {
-	
+	int unite = 0, dizaine = 0, centaine = 0, millier = 0;
+
+	if (n < 0)
+	{
+		n = n * (-1);
+		_putchar('-');
+	}
+	if (n >= 1000 || n <= -1000)
+	{
+		millier = n / 1000;
+		_putchar(millier + '0');
+	}
+	if (n >= 100 || n <= -100)
+	{
+		centaine = (n % 1000) / 100;
+		_putchar(centaine + '0');
+	}
+	if (n >= 10 || n <= -10)
+	{
+		dizaine = (n % 100) / 10;
+		_putchar(dizaine + '0');
+	}
+	unite = n % 10;
+	_putchar(unite + '0');
 }
