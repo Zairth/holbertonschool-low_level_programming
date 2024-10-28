e71828d7e295017d927d5dd7ae0555d7658a98a0
Zairth <mano.gimenez@hotmail.com>
2024-10-28 09:08:23 +0000

    g

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+*mul - Write a function that multiplies two integers and returns the result.
+*@a: variables that contains the number to operate & return the result
+*@b: variable that contain the number to operate & return the result
+*Return: return the result, an int.
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
