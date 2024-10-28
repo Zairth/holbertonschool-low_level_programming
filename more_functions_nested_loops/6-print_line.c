d5c5f7d6415c2ebe80f7055a6b0325691f2c200a
Zairth <mano.gimenez@hotmail.com>
2024-10-28 09:49:05 +0000

    g

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_line - prints n times underscore character to create a line
+*@n: the width of the line
+*Return: return void
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
