3ebee52452c4f02bcd51cd22e2db7bfe49f891b4
Zairth <mano.gimenez@hotmail.com>
2024-10-28 18:06:36 +0000

    g

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -5,62 +5,63 @@
 *@n: the integer to print
 *Return: void return
 */
-void print_number(int n)
+void print_number(long n)
 {
-	int unite = 0, dizaine = 0, centaine = 0, millier = 0, dizaine_millier = 0;
-	int centaine_millier = 0, million = 0, dizaine_million = 0;
-	int centaine_million = 0, milliard = 0;
+	long unite = 0, dizaine = 0, centaine = 0, millier = 0, dizaine_millier = 0;
+	long centaine_millier = 0, million = 0, dizaine_million = 0;
+	long centaine_million = 0, milliard = 0;
+	long new_n = n;
 
-	if (n < 0)
+	if (new_n < 0)
 	{
-		n = n * (-1);
+		new_n = new_n * (-1);
 		_putchar('-');
 	}
-	if (n >= 1000000000)
+	if (new_n >= 1000000000)
 	{
-		milliard = n / 1000000000;
+		milliard = new_n / 1000000000;
 		_putchar(milliard + '0');
 	}
-	if (n >= 100000000)
+	if (new_n >= 100000000)
 	{
-		centaine_million = (n % 1000000000) / 100000000;
+		centaine_million = (new_n % 1000000000) / 100000000;
 		_putchar(centaine_million + '0');
 	}
-	if (n >= 10000000)
+	if (new_n >= 10000000)
 	{
-		dizaine_million = (n % 100000000) / 10000000;
+		dizaine_million = (new_n % 100000000) / 10000000;
 		_putchar(dizaine_million + '0');
 	}
-	if (n >= 1000000)
+	if (new_n >= 1000000)
 	{
-		million = (n % 10000000) / 1000000;
+		million = (new_n % 10000000) / 1000000;
 		_putchar(million + '0');
 	}
-	if (n >= 100000)
+	if (new_n >= 100000)
 	{
-		centaine_millier = (n % 1000000) / 100000;
+		centaine_millier = (new_n % 1000000) / 100000;
 		_putchar(centaine_millier + '0');
 	}
-	if (n >= 10000)
+	if (new_n >= 10000)
 	{
-		dizaine_millier = (n % 100000) / 10000;
+		dizaine_millier = (new_n % 100000) / 10000;
 		_putchar(dizaine_millier + '0');
 	}
-	if (n >= 1000)
+	if (new_n >= 1000)
 	{
-		millier = (n % 10000) / 1000;
+		millier = (new_n % 10000) / 1000;
 		_putchar(millier + '0');
 	}
-	if (n >= 100)
+	if (new_n >= 100)
 	{
-		centaine = (n % 1000) / 100;
+		centaine = (new_n % 1000) / 100;
 		_putchar(centaine + '0');
 	}
-	if (n >= 10)
+	if (new_n >= 10)
 	{
-		dizaine = (n % 100) / 10;
+		dizaine = (new_n % 100) / 10;
 		_putchar(dizaine + '0');
 	}
-	unite = n % 10;
+	unite = new_n % 10;
 	_putchar(unite + '0');
 }
