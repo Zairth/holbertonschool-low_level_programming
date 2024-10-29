952cfa911b35b8a63b5f66eee8fe697ed641293e
Zairth <mano.gimenez@hotmail.com>
2024-10-29 14:45:08 +0000

    g

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -9,7 +9,11 @@ int main(void)
 {
     char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    str = "Holberton!";
+    print_rev(str);
+    str = "Lorem ipsum...";
+    print_rev(str);
+    str = "";
     print_rev(str);
     return (0);
 }
