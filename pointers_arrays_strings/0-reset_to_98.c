c5b8fa533494053f02fc5feec7ba13df39ae805a
Zairth <mano.gimenez@hotmail.com>
2024-10-29 13:14:59 +0000

    g

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+*reset_to_98 - updates the value it points to to 98
+*@n: variable that contain the adress memory where i want to change the value
+*Return: no return, void function
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
