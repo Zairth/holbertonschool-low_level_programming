92cf0d8480048ee67497890be541d6b446968875
Zairth <mano.gimenez@hotmail.com>
2024-10-29 14:27:46 +0000

    g

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
 *print_rev -  prints a string, in reverse, followed by a new line.
 *@s: string variable
@@ -5,5 +7,14 @@
 */
 void print_rev(char *s)
 {
-	
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	while (s[i] >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
 }
