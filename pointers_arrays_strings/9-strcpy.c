aee3ef83b87fec7c8cb1498a7910f02f5b05be37
Zairth <mano.gimenez@hotmail.com>
2024-10-30 14:22:46 +0000

    g

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*_strcpy - copies the string pointed to by src, to dest
+*@dest: the destination adress to copy the str
+*@src: the src value to copy to the new adress destination
+*Return: char return dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	
+}
