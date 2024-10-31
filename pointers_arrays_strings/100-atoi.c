779411efacbb5d67836818118b73523e955bec26
Zairth <mano.gimenez@hotmail.com>
2024-10-31 11:05:40 +0000

    g

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,5 +7,35 @@
 */
 int _atoi(char *s)
 {
+	int i = 0, j = 0;
+	int soustract = 0;
+	int tmp = 0;
+	int result = 0;
 
+	while (s[i] < '0' || s[i] > '9')
+	{
+		if (s[i] == '-')
+			soustract++;
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (j == 0)
+		{
+			result = s[i] - '0';
+			j++;
+		}
+		else
+		{
+			tmp = s[i] - '0';
+			result = result * 10 + tmp;
+		}
+		i++;
+	}
+
+	if (soustract % 2 != 0)
+		result = result * -1;
+
+	return (result);
 }
