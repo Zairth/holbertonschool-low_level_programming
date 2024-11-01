6b8ed3ba90bc82f104da956087252eb053399d7f
Zairth <mano.gimenez@hotmail.com>
2024-11-01 11:27:23 +0000

    g

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,10 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int value = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		if (s1[i] > s2[i])
-			value++;
-		else
-			value--;
-		i++;
-	}
+	if (s1[i] > s2[i])
+		value = s1[i] - s2[i];
+	else if (s1[i] < s2[i])
+		value = s1[i] - s2[i];
+
 	return (value);
 }
