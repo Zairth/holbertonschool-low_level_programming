444af4ffc474642a1173fc3a5a1035f9fa2d9bad
Zairth <mano.gimenez@hotmail.com>
2024-11-01 14:39:53 +0000

    g

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+*string_toupper - checks for lowercase character
+*@c: pointer that contain the array
+*Return: return the new array
+*/
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] = c[i] - 32;
+		i++;
+	}
+	return (c);
+}
