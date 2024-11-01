3d1868aeab76a2bab58dc57b3a86588f37dcd660
Zairth <mano.gimenez@hotmail.com>
2024-11-01 11:34:56 +0000

    g

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+    char s1[] = "Holberton"; 
+    char s2[] = "Holbeerton"; 
+    _strcmp(s1, s2);
 
     printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
     return (0);
 }
--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,24 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 	int value = 0;
+	int found_diff = 0;
 
-	if (s1[i] > s2[i])
-		value = s1[i] - s2[i];
-	else if (s1[i] < s2[i])
-		value = s1[i] - s2[i];
-
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (found_diff == 0)
+		{
+			if (s1[i] > s2[i])
+			{
+				value = s1[i] - s2[i];
+				found_diff++;
+			}
+			else if (s1[i] < s2[i])
+			{
+				value = s1[i] - s2[i];
+				found_diff++;
+			}
+		}
+		i++;
+	}
 	return (value);
 }
