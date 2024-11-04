4fc428f40fb0e880cc4d35f2d7111756104d417d
Zairth <mano.gimenez@hotmail.com>
2024-11-04 08:35:46 +0000

    ROT13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+/**
+*rot13 - encode a string with ROT13
+*@c: pointer that contain the array
+*Return: char return the new array
+*/
+char *rot13(char *c)
+{
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j = 0;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		while (j < 52)
+		{
+			if (input[j] == c[i])
+			{
+				c[i] = output[j];
+				break;
+			}
+			j++;
+		}
+	}
+	return (c);
+}
