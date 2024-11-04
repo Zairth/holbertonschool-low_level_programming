eaa2b952ff6cc8c746489ac3925782ece2a1b4c5
Zairth <mano.gimenez@hotmail.com>
2024-11-04 15:49:46 +0100

    g

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+/**
+ *_memcpy - fill  memory with a constant byte.
+ *@n: number of character to copy to dest
+ *@src: the src character to copy n times to the dest var his memory area
+ *@dest: the destination strings return
+ *Return: char return
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
