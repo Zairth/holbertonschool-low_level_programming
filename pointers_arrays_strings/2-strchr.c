3aa896bddd8aea417550bb003dab740b8a2a29ff
Zairth <mano.gimenez@hotmail.com>
2024-11-04 17:05:07 +0100

    g

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+/**
+ *  *_strchr - locates a character in a string.
+ *   *@s: the string to verify
+ *    *@c: the char to compare
+ *     *Return: char return
+ *      **/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			s = s + i;
+			return (s);
+		}
+	}
+	return (0);
+}
