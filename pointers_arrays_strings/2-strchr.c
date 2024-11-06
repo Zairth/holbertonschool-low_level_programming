186fc394a642c13af182897162752999ddd63d0f
Zairth <mano.gimenez@hotmail.com>
2024-11-06 17:07:13 +0100

    g

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,9 +8,6 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	if (*s == '\0')
-		return (s);
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
@@ -19,5 +16,9 @@ char *_strchr(char *s, char c)
 			return (s);
 		}
 	}
+
+	if (c == '\0')
+		return (&s[i]);
+
 	return (0);
 }
