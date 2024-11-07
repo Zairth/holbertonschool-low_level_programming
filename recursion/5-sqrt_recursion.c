2151a58b943ccb82c78fade2859290c3f720d757
Zairth <mano.gimenez@hotmail.com>
2024-11-07 19:33:30 +0100

    g

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+/**
+*_sqrt_recursion - returns the natural square root of a number.
+*@n: the number to get the pow
+*Return: int return
+*/
+
+/* Trouver la multiplication par un meme nombre qui est égale à n */
+int _sqrt_recursion(int n)
+{
+	
+}
