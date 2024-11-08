add267444e639f67b1f0be927991199c86c9de32
Zairth <mano.gimenez@hotmail.com>
2024-11-08 23:28:36 +0100

    g

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc: number of parameter
+ *@argv: the array of parameter
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	int a = 0, b = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", a * b);
+
+	return (0);
+}
