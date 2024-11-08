4c12c380ba3ee196d148a37325fdb256879394e5
Zairth <mano.gimenez@hotmail.com>
2024-11-08 21:28:05 +0100

    g

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *@argc: number of parameter
+ *@argv: the array of parameter
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	printf("Parametre %d : %s\n", argc, argv[0]);
+	return (0);
+}
