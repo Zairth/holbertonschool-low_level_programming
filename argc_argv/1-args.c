106a7c9572d4c2e1025d217d7b20dac4685dbfad
Zairth <mano.gimenez@hotmail.com>
2024-11-08 21:49:24 +0100

    g

---
+++ argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *@argc: number of parameter
+ *@argv: the array of parameter
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	for (i = 0; argv[i] <= argc; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
+
+
