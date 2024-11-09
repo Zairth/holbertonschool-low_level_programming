55aaf7548d6da4ec5226ed9259f510bccd957fdc
Zairth <mano.gimenez@hotmail.com>
2024-11-09 12:48:00 +0100

    g

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *@argc: number of parameter
+ *@argv: the array of parameter
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, result = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] < '0' && argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			result += atoi(argv[i]);
+		}
+		printf("%d\n", result);
+	} else
+		printf("0\n");
+
+	return (0);
+}
