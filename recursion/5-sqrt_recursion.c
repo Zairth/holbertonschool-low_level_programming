3d4f1d97c27a61e5c88f96344f9897acbd8f5976
Zairth <mano.gimenez@hotmail.com>
2024-11-09 13:38:22 +0100

    g

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,11 +1,24 @@
 /**
+*natural_square - returns the natural square root of a number.
+*@n: the number to get the natural square root
+*@count: the number to test
+*Return: int return
+*/
+int natural_square(int n, int count)
+{
+	if (count * count == n)
+		return (count);
+	if (count > n / 2)
+		return (-1);
+	return (natural_square(n, count + 1));
+}
+
+/**
 *_sqrt_recursion - returns the natural square root of a number.
 *@n: the number to get the pow
 *Return: int return
 */
-
-/* Trouver la multiplication par un meme nombre qui est égale à n */
 int _sqrt_recursion(int n)
 {
-	
+	return (natural_square(n, 0));
 }
