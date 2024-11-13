7932368ccc1ae81d80bf1ee23f95ade1aa2ef7c1
Zairth <mano.gimenez@hotmail.com>
2024-11-13 10:36:04 +0100

    g

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,6 +10,10 @@
 char *create_array(unsigned int size, char c)
 {
 	char *new_arr = malloc(sizeof(char) * size);
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		new_arr[i] = c;
 
 	if (new_arr == NULL || size == 0)
 		return (NULL);
