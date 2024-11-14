d6c61b437cb00cd1c95df32100ef09b682084661
Zairth <mano.gimenez@hotmail.com>
2024-11-14 11:08:21 +0100

    g

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,13 +15,13 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = (int *)malloc(sizeof(int *) * height);
+	grid = (int **)malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(sizeof(int) * width);
+		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 			return (NULL);
 	}
