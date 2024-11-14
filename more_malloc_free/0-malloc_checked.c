280009b71f7abbe5b90697b8ae7cdddfed379972
Zairth <mano.gimenez@hotmail.com>
2024-11-14 15:28:52 +0100

    g

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*malloc_checked - allocates memory using malloc
+*@b: variable that contain the char
+*Return: void return
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *a = NULL;
+
+	a = malloc(b);
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
