12af2d19c2a4ddded936959a1f50b7a46a0b664e
Zairth <mano.gimenez@hotmail.com>
2024-11-15 19:50:09 +0100

    g

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,12 +13,11 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
+		printf("Ok\n");
+	else
 	{
-		printf("Ok");
-		exit (1);
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
 }
