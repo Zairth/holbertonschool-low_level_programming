77e97edee13e7edea149451010354cec3de7a626
Zairth <mano.gimenez@hotmail.com>
2024-11-15 09:25:16 +0100

    g

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			s3[i] = s1[i];
 		for (x = 0; x <= c; x++)
 			s3[i + x] = s2[x];
-		s3[i + x + 1] = '\0';
 	}
 	else
 	{
@@ -45,7 +44,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			s3[i] = s1[i];
 		for (x = 0; x <= n; x++)
 			s3[i + x] = s2[x];
-		s3[i + x + 1] = '\0';
 	}
 
 	return (s3);
