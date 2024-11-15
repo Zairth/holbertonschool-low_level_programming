1443515bad14da78497ad94ca9b394518b364e9c
Zairth <mano.gimenez@hotmail.com>
2024-11-15 16:39:01 +0100

    g

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *struct dog - new type struct dog
+ *@name: The name of the dog.
+ *@age: The age of the dog.
+ *@owner: The owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
