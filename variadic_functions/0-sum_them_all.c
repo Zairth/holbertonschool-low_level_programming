9a0063cd7a9592dfbe6dc82c697f5f7ecdba950d
Zairth <mano.gimenez@hotmail.com>
2024-11-20 12:09:57 +0100

    g

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+*sum_them_all - return the sum of all variadic argument
+*@n: number of variadic argument
+*@...: variadic arguments initialization
+*Return: int return
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	
+}
