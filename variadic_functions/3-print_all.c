ab78d8a4abfe588af9332a7e7bf4b1e452eb90e8
Zairth <mano.gimenez@hotmail.com>
2024-11-21 12:35:16 +0100

    g

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,7 +11,7 @@
 */
 void print_all(const char * const format, ...)
 {
-	int hint, i = 0;
+	int hint, i = 0, acc_format;
 	char charr;
 	char *str;
 	float floot;
@@ -21,28 +21,33 @@ void print_all(const char * const format, ...)
 
 	while (format[i] != '\0')
 	{
+		acc_format = 0;
 		if (format[i] == 'c')
 		{
 			charr = va_arg(args, int);
 			printf("%c", charr);
+			acc_format++;
 		}
 		else if (format[i] == 'i')
 		{
 			hint = va_arg(args, int);
 			printf("%d", hint);
+			acc_format++;
 		}
 		else if (format[i] == 'f')
 		{
 			floot = va_arg(args, double);
 			printf("%f", floot);
+			acc_format++;
 		}
 		else if (format[i] == 's')
 		{
 			str = va_arg(args, char*);
 			printf("%s", str);
+			acc_format++;
 		}
 
-		if (str == NULL || format[i + 1] != '\0')
+		if ((str == NULL || format[i + 1] != '\0') && (acc_format > 0))
 		{
 			printf(", ");
 			str = "(nil)";
