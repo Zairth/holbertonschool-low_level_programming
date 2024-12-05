c4cad62c082a8c7432af376433a432c59f870fbc
Zairth <mano.gimenez@hotmail.com>
2024-12-05 11:48:29 +0100

    g

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,8 +13,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *new_node;
-	dlistint_t *ptr;
+	dlistint_t *new_node, *ptr;
 
 	if (h == NULL)
 		return (NULL);
@@ -23,24 +22,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
-	new_node->n = n;
 
 	if (idx == 0)
 	{
 		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
-
 	ptr = *h;
-	while (i < idx - 1  && ptr != NULL)
+	while (ptr != NULL && i < idx - 1)
 	{
 		ptr = ptr->next;
 		i++;
 	}
-
+	if (ptr == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->next = ptr->next;
 	if (ptr->next != NULL)
 		ptr->next->prev = new_node;
