09181af0df4b1e865e3416afb6b1df10e19033e0
Zairth <mano.gimenez@hotmail.com>
2024-12-05 11:31:22 +0100

    g

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->next = NULL;
 	new_node->prev = NULL;
 	new_node->n = n;
@@ -40,5 +40,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	ptr->next->prev = new_node;
 	ptr->next = new_node;
 
-	return(new_node);
+	return (new_node);
 }
