8285145d0b8b3764a8484cdf1210ab3736c7c954
Zairth <mano.gimenez@hotmail.com>
2024-12-05 11:42:10 +0100

    g

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -29,9 +29,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new_node->next = (*h);
-		(*h)->prev = new_node;
-		(*h) = new_node;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
 		return (new_node);
 	}
 
