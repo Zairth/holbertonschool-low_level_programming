6d58e45f4ad59c82296940f3e431f7306ae1feea
Zairth <mano.gimenez@hotmail.com>
2024-12-08 22:47:43 +0100

    g

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,26 +11,26 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	ssize_t bytesWrited, i = 0;
-
-	while (text_content[i] != '\0')
-		i++;
-
-	if (i == 0)
-		text_content = "";
+	int i = 0, file;
 
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (file == -1)
 		return (-1);
 
-	bytesWrited = write(file, text_content, i);
-	if (bytesWrited == -1)
-		return (-1);
+	write(file, text_content, i);
 
-	close(file);
 	return (1);
 }
