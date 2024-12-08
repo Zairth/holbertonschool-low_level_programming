c9860b370869452757e03c3c08660ec56bbe73c6
Zairth <mano.gimenez@hotmail.com>
2024-12-08 22:45:15 +0100

    g

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,8 +11,8 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i = 0;
-	ssize_t bytesWrited;
+	int file;
+	ssize_t bytesWrited, i = 0;
 
 	while (text_content[i] != '\0')
 		i++;
