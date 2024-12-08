08d0a4ee28805dc3b205974afacb277167e68358
Zairth <mano.gimenez@hotmail.com>
2024-12-08 22:39:51 +0100

    g

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -27,9 +27,7 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	bytesWrited = write(file, text_content, i);
-	if (bytesWrited == -1)
-		return (-1);
+	write(file, text_content, i);
 
 	close(file);
 	return (1);
