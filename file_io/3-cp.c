7b32c93575676bc180c63d256963c979b2b3b795
Zairth <mano.gimenez@hotmail.com>
2024-12-09 12:36:32 +0100

    g

---
+++ file_io/3-cp.c
@@ -0,0 +1,82 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *@argc: number of argument
+ *@argv: the array of argument
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		perror("Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	cp_from_to_file(argv[1], argv[2]);
+
+	return (0);
+}
+
+/**
+*cp_from_to_file - appends text at the end of a file.
+*@from_filename: the file name
+*@to_file: the destination file
+*Return: void
+*/
+void cp_from_to_file(const char *from_filename, const char *to_file)
+{
+	int fd_from, fd_to, closed_from, closed_to;
+	char *buffer;
+	ssize_t readed;
+	ssize_t writed;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+		return;
+
+	fd_from = open(from_filename, O_RDONLY);
+	if (fd_from == -1)
+	{
+		perror("Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+	readed = read(fd_from, buffer, 1024);
+	if (readed < 0)
+	{
+		perror("Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(98);
+	}
+
+	fd_to = open(to_file, O_CREAT | O_WRONLY, 0664);
+	if (fd_to == -1)
+	{
+		perror("Error: Can't read from file NAME_OF_THE_FILE\n");
+		exit(99);
+	}
+	writed = write(fd_to, buffer, readed);
+	if (writed < 0 || writed != readed)
+	{
+		perror("Error: Can't write to NAME_OF_THE_FILE\n");
+		exit(99);
+	}
+
+	closed_from = close(fd_from);
+	if (closed_from == -1)
+	{
+		perror("Error: Can't close fd FD_VALUE");
+		exit(100);
+	}
+	closed_to = close(fd_to);
+	if (closed_to == -1)
+	{
+		perror("Error: Can't close fd FD_VALUE");
+		exit(100);
+	}
+	free(buffer);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -5,9 +5,11 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void cp_from_to_file(const char *from_filename, const char *to_file);
 #endif
