1ab4359c8a89515524c97a75f4b5f29107ab3c09
Zairth <mano.gimenez@hotmail.com>
2024-12-09 13:15:36 +0100

    g

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -69,7 +69,7 @@ void cp_from_to_file(const char *from_filename, const char *to_file)
 	fd_to = open(to_file, O_CREAT | O_WRONLY, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", to_file);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", to_file);
 		exit(99);
 	}
 	writed = write(fd_to, big_buffer, total_read);
